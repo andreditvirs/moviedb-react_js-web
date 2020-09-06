eb96015b8998207beb44c1bd997708afd2ff04cd
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-06 13:18:16 +0700

    Updates

---
+++ precache-manifest.1802620d1d20ca2fdd27aa9bd5d46161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc9b7ac996bc12d541f481200365dab",
+    "url": "/moviedb-react_js-web/index.html"
+  },
+  {
+    "revision": "36a9a54a2baf58c855aa",
+    "url": "/moviedb-react_js-web/static/css/main.ea7c7793.chunk.css"
+  },
+  {
+    "revision": "62aa47f52db4102f32f3",
+    "url": "/moviedb-react_js-web/static/js/2.03928c89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb-react_js-web/static/js/2.03928c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a9a54a2baf58c855aa",
+    "url": "/moviedb-react_js-web/static/js/main.d45cddf2.chunk.js"
+  },
+  {
+    "revision": "377dcf0da13cb4eaedc2",
+    "url": "/moviedb-react_js-web/static/js/runtime-main.d50fab46.js"
+  }
+]);
